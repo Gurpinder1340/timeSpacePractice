052f7b59b091ef364b47cbb48bfce0f27a37d1db
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-04-22 20:10:38 -0700

    added 1 test

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -6,5 +6,20 @@ public class PracticeTest {
   
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
+  
+
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
 }
+
+@Test
+public void testMostTimeEfficient() {
+  // Arrange
+  int [] nums = {2};
+
+  // Act
+  int result = Practice.mostCommonTimeEfficient(nums);
+
+  // Assert
+  assertEquals(4, result);
+}
+
